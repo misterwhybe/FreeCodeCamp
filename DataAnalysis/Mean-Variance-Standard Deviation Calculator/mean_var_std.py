39f0c6175473b80d737488e88387f4940d428d97
misterwhybe <44019712+misterwhybe@users.noreply.github.com>
2023-05-28 22:15:00 +0200

    Create mean_var_std.py

---
+++ DataAnalysis/Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def calculate(numbers):
+    if len(numbers) != 9:
+      raise ValueError("List must contain nine numbers.")
+    # Convert the list into a 3x3 numpy array
+    matrix = np.array(numbers).reshape(3, 3)
+
+    # Calculate the mean, variance, standard deviation, max, min, and sum
+    result = {
+        'mean': 
+            [matrix.mean(axis=0).tolist(),
+            matrix.mean(axis=1).tolist(),
+            matrix.mean()]
+        ,
+        'variance': 
+            [matrix.var(axis=0).tolist(),
+            matrix.var(axis=1).tolist(),
+            matrix.var()]
+        ,
+        'standard deviation': 
+            [matrix.std(axis=0).tolist(),
+            matrix.std(axis=1).tolist(),
+            matrix.std()]
+        ,
+        'max': 
+            [matrix.max(axis=0).tolist(),
+            matrix.max(axis=1).tolist(),
+            matrix.max()]
+        ,
+        'min': 
+            [matrix.min(axis=0).tolist(),
+            matrix.min(axis=1).tolist(),
+             matrix.min()]
+        ,
+        'sum': 
+            [matrix.sum(axis=0).tolist(),
+            matrix.sum(axis=1).tolist(),
+            matrix.sum()]
+        
+    }
+
+    return result
